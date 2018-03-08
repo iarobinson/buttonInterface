fb4c3f49707b5ff6f287b3c54059e98798e4cd2d
Ian <ian (at) ianrobinson.net>
2018-03-08 10:13:19 -0500

    Add css to visually see button selected, some JS functionality built in to button operations

---
+++ button.js
@@ -0,0 +1,114 @@
+// Once page is fully loaded, we commence with the program
+// *This could be refactored with a helper function to avoid the non-DRY elements
+window.onload = function() {
+  // Applies each set of buttons to a variable and loops over each
+  
+  // First we do control links
+  var controlLinks = document.querySelectorAll("button.control");
+  for (var i = 0; i < controlLinks.length; i += 1) {
+    controlLinks[i].onclick = handleControl;
+  }
+  
+  // Same for Effects
+  var effectLinks = document.querySelectorAll("button.effect");
+  for (var i = 0; i < effectLinks.length; i += 1) {
+    effectLinks[i].onclick = setEffect;
+  }
+  
+  // Same for Selecting Video Links
+  var videoLinks = document.querySelectorAll("button.videoSelection");
+  for (var i = 0; i < videoLinks.length; i += 1) {
+    videoLinks[i].onclick = setVideo;
+  }
+  
+  // These are helper functions that visually press or depress buttons
+  pushUnpushButtons("video1", []);
+  pushUnpushButtons("normal", []);
+}
+
+// This function manages when buttons which control the video player
+function handleControl(click) {
+  var id = click.target.getAttribute("id");
+  // Using the above variable constructor, we get strings based on the value of the button pressed
+  // All of this is cosmetic and changes the way we display the buttons only.
+  if (id == "play") {
+    pushUnpushButtons("play", ["pause"]);
+  } else if (id == "pause") {
+    pushUnpushButtons("pause", ["play"]);
+  } else if (id == "loop") {
+    if (isButtonPushed("loop")) {
+      pushUnpushButtons("", ["loop"]);
+    } else {
+      pushUnpushButtons("loop", []);
+    }
+  } else if (id == "mute") {
+    if (isButtonPushed("mute")) {
+      pushUnpushButtons("", ["mute"]);
+    } else {
+      pushUnpushButtons("mute", []);
+    }
+  }
+}
+
+function isButtonPushed() {
+  
+}
+
+function setEffect(click) {
+  // Someone clicked a button and this sets an ID variable to that button's id attribue 
+  var id = click.target.getAttribute("id");
+  
+  // Now we run through if statements to decide which button was clicked and how the graphics should respond
+  if (id == "normal") {
+    pushUnpushButtons("normal", ["western", "noir", "scifi"]);
+  } else if (id == "western") {
+    pushUnpushButtons("western", ["normal", "noir", "scifi"]);
+  } else if (id == "noir") {
+    pushUnpushButtons("noir", ["normal", "western", "scifi"]);
+  } else if (id == "scifi") {
+    pushUnpushButtons("scifi", ["normal", "western", "scifi"]);
+  }
+}
+
+// Set Video function does the same as set Effect
+// It just applies a variable to the id that has been selected and performs a function based on that selection
+function setVideo(click) {
+  var id = click.target.getAttribute("id");
+  
+  if (id == "video1") {
+    pushUnpushButtons("video1", ["video2"]);
+  } else {
+    pushUnpushButtons("video2", ["video1"]);
+  }
+}
+
+// Here we are doing the logic which seems so obvious in analoge environments.
+// We are passed the button clicked, and should have all other buttons related become unpushed.
+function pushUnpushButtons(push, unpush) {
+  // This just means, if a button is pushed we will do something
+  if (push != "") {
+    // Anchor variable set to the button which was pushed
+    var anchor = document.getElementById(push);
+    // The class of the anchor will store the string " selected" if the button is pushed
+    var theClass = anchor.getAttribute("class");
+    // This just says, 'is this class pushed'? or does it contain the "selected string"
+    if (!theClass.indexOf("selected") >= 0) {
+      // If the button isn't selected, it makes it selected
+      theClass += " selected";
+      // Resets the anchor back to 'control' 'effect' or "videoSelection" depeding on what button pressed
+      anchor.setAttribute("class", theClass);
+      console.log(theClass);
+      // var newImage = "url(images/" + push + "pressed.png)";
+      // anchor.style.backgroundImage = newImage;
+    }
+  }
+  
+  for (var i = 0; i < unpush.length; i += 1) {
+    anchor = document.getElementById(unpush[i]);
+    theClass = anchor.getAttribute("class");
+    if (theClass.indexOf("selected") >= 0) {
+      // theClass = theClass.replace("selected". "");
+    }
+  }
+  
+}
